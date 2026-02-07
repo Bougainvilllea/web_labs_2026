67c7790f99368e07f4ae4cfccbb653b990da7cd9
l <liza.berdnikova@gmail.com>
2026-02-07 22:54:08 +0700

    lab1

--- src/task3.kt
+++ src/task3.kt
@@ -1,5 +1,7 @@
-fun separation_by_space_filter_alf_distinct(input: Array<String>?)){
-    input?.sorted()?.distinct()?.forEach {println(it)}
+fun separation_by_space_filter_alf_distinct(input: Array<String>?){
+    input?.sorted()
+        ?.distinct()
+        ?.forEach {println(it)}
 }
 
 fun main(args: Array<String>)){
